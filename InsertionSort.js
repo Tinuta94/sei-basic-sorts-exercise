887c132f719d7f425569ad15b8480ca90ed69260
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-09-02 16:29:50 -0400

    hw

---
+++ InsertionSort.js
@@ -0,0 +1,32 @@
+function insertionSort (items) {
+  // Loop through each element
+  const len = items.length;
+        for (i = 0; i < len; i++) { 
+          let el = items[i]
+          let j;
+        
+          for(j = i -1; j >= 0 && items[j] > el; j--) {
+         items[j + 1] = items[j];
+          }
+          items[j + 1] = el;
+      }  
+      return items;
+    }
+
+  // store the current item value so it can be placed correctly
+  // in the sorted portion of the array
+ 
+
+  // Loop backward through the sorted portion of the array
+  // and scoot everything over until you find the right place to
+  // insert the value you're working with
+
+  // Copy each item to the next slot over, as long as the value is smaller
+  // than the item in the sorted array we're looking at (items[j] > value)
+
+  // We can now insert the item in its sorted location
+
+  // Remember to return the list!
+  
+
+module.exports = insertionSort;
